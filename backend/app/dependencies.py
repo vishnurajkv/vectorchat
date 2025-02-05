ff99556ec25e2a2cce63a1a5a08fb9704b191e37
Vishnu <vishnuraj.k.v0@gmail.com>
2025-02-05 11:45:09 +0100

    Update dependencies.py

--- backend/app/dependencies.py
+++ backend/app/dependencies.py
@@ -1,1 +1,16 @@
-0
+from langchain_huggingface import HuggingFaceEmbeddings, HuggingFaceEndpoint
+from .config import get_settings
+
+settings = get_settings()
+
+def get_embeddings():
+    return HuggingFaceEmbeddings(model_name="all-mpnet-base-v2")
+
+def get_llm():
+    return HuggingFaceEndpoint(
+        repo_id=settings.model_repo_id,
+        task="text-generation",
+        temperature=settings.temperature,
+        max_length=settings.max_length,
+        token=settings.huggingface_api_token
+    )
