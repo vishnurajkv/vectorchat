a19b75345e6bf09b67d01f9c8a12e015b6f8fae1
Vishnu <vishnuraj.k.v0@gmail.com>
2025-02-05 11:49:33 +0100

    Update ChatInterface.jsx

--- frontend/src/components/ChatInterface.jsx
+++ frontend/src/components/ChatInterface.jsx
@@ -1,1 +1,8 @@
+import React, { useState } from 'react';
+import FileUpload from './FileUpload';
+import ChatMessage from './ChatMessage';
+import { uploadFile, sendMessage } from '../services/api';
+import { MessageTypes } from '../utils/constants';
 
+const ChatInterface = () => {
+  const [sessionId, set
